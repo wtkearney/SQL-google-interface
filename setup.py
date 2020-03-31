0ab0cbed8d7a6be9e0af0478638def97d2d9359b
wkearney <wkearney@pps.net>
2020-03-31 13:42:08 -0700

    Updated version

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="SQL-google-interface", # Replace with your own username
-    version="1.8",
+    version="1.9",
     author="William Kearney",
     author_email="wtkearney@gmail.com",
     description="An interface to interact between a SQL database and Google Drive folders",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/wtkearney/SQL-google-interface",
-    download_url = 'https://github.com/wtkearney/SQL-google-interface/archive/v1.8.tar.gz',
+    download_url = 'https://github.com/wtkearney/SQL-google-interface/archive/v1.9.tar.gz',
     install_requires=[
         "pyodbc",
         "pandas",
